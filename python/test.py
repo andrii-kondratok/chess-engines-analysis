aba7a30f0e4e85a175363bc4573fd8cca9cf8539
andrii-kondratok <akondratok@kse.org.ua>
2026-05-26 15:27:18 +0300

    Built site for gh-pages

--- python/test.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-token = os.getenv("HF_TOKEN")
-if token:
-    print(f"✓ Token loaded: {token[:10]}...{token[-4:]}")
-else:
-    print("✗ Token not found!")
